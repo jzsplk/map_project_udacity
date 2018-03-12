6286e8571e1cb266f1c6ccb63708036b9ef53b53
Alex <jzsplking@gmail.com>
2018-03-12 12:25:18 +0800

    Feature： 修改搜索框css，refactor viewModel为一个类

--- js/app.js
+++ js/app.js
@@ -1,34 +1,43 @@
-var viewModel = {
-  items: [ { Name: "Park Ave Penthouse" , Id: 0}, { Name: "Chelsea Loft" , Id: 1}, { Name: "Union Square Open Floor Plan" , Id: 2}, { Name: "East Village Hip Studio" , Id: 3}, { Name: "TriBeCa Artsy Bachelor Pad" , Id: 4}, { Name: "Chinatown Homey Space" , Id: 5} ]
-};
+// global ko
+// (function() {
+	function viewModel() {
+		var self = this;
+		self.items = [ { Name: "Park Ave Penthouse" , Id: 0}, { Name: "Chelsea Loft" , Id: 1}, { Name: "Union Square Open Floor Plan" , Id: 2}, { Name: "East Village Hip Studio" , Id: 3}, { Name: "TriBeCa Artsy Bachelor Pad" , Id: 4}, { Name: "Chinatown Homey Space" , Id: 5} ];
+		self.Query = ko.observable('');
+	  	self.searchResults = ko.computed(function() {
+		    var q = self.Query().toLowerCase();
+		    if(!q) {
+		    	return self.items;
+		    } else {
+		    	    return self.items.filter(function(i) {
+				      return i.Name.toLowerCase().indexOf(q) != -1;
+				    });
+		    }
 
-viewModel.Query = ko.observable('');
+		});
 
-viewModel.searchResults = ko.computed(function() {
-    var q = viewModel.Query().toLowerCase();
-    if(!q) {
-    	return viewModel.items;
-    } else {
-    	    return viewModel.items.filter(function(i) {
-		      return i.Name.toLowerCase().indexOf(q) != -1;
-		    });
-    }
+	}
 
-});
 
-ko.applyBindings(viewModel);
+	var myViewModel = new viewModel();
+	ko.applyBindings(myViewModel);
 
-//menu controll
-var menu = document.querySelector('#menu');
-var map = document.querySelector('.map');
-var drawer = document.querySelector('.filter');
+	//menu controll
+	var menu = document.querySelector('#menu');
+	var map = document.querySelector('.map');
+	var drawer = document.querySelector('.filter');
+	var search = document.querySelector('.form-control');
 
-menu.addEventListener('click', function(e) {
-	drawer.classList.toggle('open');
-	e.stopPropagation();
-});
-map.addEventListener('click', function() {
-	drawer.classList.remove('open');
-});
+	menu.addEventListener('click', function(e) {
+		drawer.classList.toggle('open');
+		search.classList.toggle('open');
+		e.stopPropagation();
+	});
+	map.addEventListener('click', function() {
+		drawer.classList.remove('open');
+		search.classList.remove('open');
+	});
 
+
+// })();
 
--- js/map.js
+++ js/map.js
@@ -66,7 +66,7 @@ function initMap() {
     // This function will loop through the markers array and display them all.
     function showListings() {
       var bounds = new google.maps.LatLngBounds();
-      var mapSearchResults = viewModel.searchResults().map(function(e) {
+      var mapSearchResults = myViewModel.searchResults().map(function(e) {
       	return e.Name.toLowerCase();
       });
       // Extend the boundaries of the map for each marker and display the marker
@@ -119,16 +119,3 @@ function populateInfoWindow(marker, infowindow) {
   }
 }
 
-//toggleInfoWindow
-function toggleInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
-    infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick',function(){
-      infowindow.setMarker = null;
-    });
-  }
-}
